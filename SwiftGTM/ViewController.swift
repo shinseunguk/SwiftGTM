54d27915fe365d5a1c147a495d5c0654f0e54df0
신승욱 <ukseung.dev@incross.com>
2023-11-29 10:15:00 +0900

    Initial Commit

---
+++ SwiftGTM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGTM
+//
+//  Created by ukseung.dev on 11/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
