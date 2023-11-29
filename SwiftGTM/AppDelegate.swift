5463660449631a32313d3508db4356deb1b7af14
신승욱 <ukseung.dev@incross.com>
2023-11-29 14:18:38 +0900

    feat: GTM 연동

--- SwiftGTM/AppDelegate.swift
+++ SwiftGTM/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure() // Google Analytics를 위한 코드작성
         return true
     }
 
--- SwiftGTM/ViewController.swift
+++ SwiftGTM/ViewController.swift
@@ -6,14 +6,89 @@
 //
 
 import UIKit
+import Then
+import Firebase
+import SnapKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+protocol ViewAttributes {
+    func setUI()
+    func setAttributes()
+    func bindRx()
+}
 
+class ViewController: UIViewController, ViewAttributes {
+    let disposeBag = DisposeBag()
+    
+    lazy var addButton = UIButton().then {
+        $0.setTitle("Add Button", for: .normal)
+        $0.backgroundColor = .systemBlue
+        $0.layer.cornerRadius = 10
+    }
+    
+    lazy var modifyButton = UIButton().then {
+        $0.setTitle("Modify Button", for: .normal)
+        $0.backgroundColor = .systemBlue
+        $0.layer.cornerRadius = 10
+    }
+    
+    lazy var blockButton = UIButton().then {
+        $0.setTitle("Block Button", for: .normal)
+        $0.backgroundColor = .systemBlue
+        $0.layer.cornerRadius = 10
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUI()
+        setAttributes()
+        bindRx()
     }
-
-
+    
+    func setUI() {
+        self.view.addSubview(addButton)
+        self.view.addSubview(modifyButton)
+        self.view.addSubview(blockButton)
+    }
+    
+    func setAttributes() {
+        addButton.snp.makeConstraints {
+            $0.bottom.equalTo(modifyButton.snp.top).offset(-40)
+            $0.width.height.centerX.equalTo(modifyButton)
+        }
+        
+        modifyButton.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.width.equalTo(200)
+            $0.height.equalTo(50)
+        }
+        
+        blockButton.snp.makeConstraints {
+            $0.top.equalTo(modifyButton.snp.bottom).offset(40)
+            $0.width.height.centerX.equalTo(modifyButton)
+        }
+    }
+    
+    func bindRx() {
+        addButton.rx.tap
+            .subscribe(onNext: {
+                Analytics.logEvent("AddEvent", parameters: nil)
+            })
+            .disposed(by: disposeBag)
+        
+        modifyButton.rx.tap
+            .subscribe(onNext: {
+                Analytics.logEvent("ModifyEvent", parameters: nil)
+            })
+            .disposed(by: disposeBag)
+        
+        blockButton.rx.tap
+            .subscribe(onNext: {
+                Analytics.logEvent("BlockEvent", parameters: nil)
+            })
+            .disposed(by: disposeBag)
+    }
 }
 
